1e6b59d12d9f2f2716e19595754a6483f80bbd5d
Theo Bardu <mattebardu@gmail.com>
2026-01-05 08:00:16 +0100

    Modificato il file di test con l'esempio di schedule con multithread. Da usare su raspy per i due job simultanei

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
 from helper_aeroGreenHouse import aeroHelper
+import yaml
+import schedule
+from time import sleep
+import threading
+import datetime
+
+
 
 ah = aeroHelper()
+CONFIG_FILE = "config.yaml" #nome del file di configurazione .yaml
+
+
+def load_config():
+    with open(CONFIG_FILE, "r") as f:
+        return yaml.safe_load(f)
+
+
+def job1():
+    config = load_config()
+    print(datetime.datetime.now(),config['T_var'])
+
+def job2():
+    config = load_config()
+    print(datetime.datetime.now(), config['dht22'])
+
+
+def run_thread(job):
+    job_thread = threading.Thread(target = job)
+    job_thread.start()
+
+
+
+schedule.every(2).seconds.do(run_thread,job1)
+schedule.every(3).seconds.do(run_thread,job2)
+
+print(datetime.datetime.now())
+while True:
+    schedule.run_pending()
+    sleep(1)
+
+
--- test.py
+++ test.py
@@ -1,21 +1,41 @@
-import yaml
-import os
 from helper_aeroGreenHouse import aeroHelper
-from numpy import arange
+import yaml
+import schedule
+from time import sleep
+import threading
+import datetime
 
 ah = aeroHelper()
-print(ah.configs['gpio_pins'])
-# tsep = 40
+configs = ah.configs
+# t = configs['gpio_pins'][0]['interval']
 
-# T = arange(-5,18)
-# for i,t in enumerate(T):
-#     f = ah.T_modifier(t)
-#     print(T[i], tsep - f*tsep)
+CONFIG_FILE = "config.yaml" #nome del file di configurazione .yaml
 
 
-# with open('config.yaml', "r") as f:
-#     config = yaml.safe_load(f)
+def load_config():
+    with open(CONFIG_FILE, "r") as f:
+        return yaml.safe_load(f)
 
-# print(config)
-# print(config['gpio_pins'][0]['name'])
-# print(config.get("config_reload_interval", 5))
+
+def job1(what):
+    config = load_config()
+    print(datetime.datetime.now(),config[what])
+
+
+
+def run_thread(job,what):
+    job_thread = threading.Thread(target = job(what))
+    job_thread.start()
+
+
+
+schedule.every(2).seconds.do(run_thread,job1,what='T_var')
+schedule.every(3).seconds.do(run_thread,job1,what='dht22')
+
+
+print(datetime.datetime.now())
+while True:
+    schedule.run_pending()
+    sleep(1)
+
+
